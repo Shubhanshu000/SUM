b48ff76dc815b707ed59c8ee2abfc411247586d7
Shubhanshu Saadhiyaan <Shubhanshu.saini.sss@gmail.com>
2021-09-23 19:55:29 +0530

    fuck you bitch

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 class sum {
   public static void main(String[] args) {
      int a=23, b=34;
-     System.out.println(" sum of two numbers " +(a+b));
+     System.out.println(" sum " +(a+b));
  }
 }
