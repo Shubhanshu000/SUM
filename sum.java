c4658ae7847a251242aa4c7f2e227bc154dae2e7
Shubhanshu Saadhiyaan <Shubhanshu.saini.sss@gmail.com>
2021-09-23 19:44:49 +0530

    public static void main doublicate bn rha tha

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 class sum {
- public static void public static void main(String[] args) {
+  public static void main(String[] args) {
      int a=23, b=34;
-     System.out.println("the sum of two numbers " +(a+b));
+     System.out.println(" sum of two numbers " +(a+b));
  }
 }
